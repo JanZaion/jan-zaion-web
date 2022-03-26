a234f6c5df2cf8b8f14437802ab340616841dbb9
ZaionCode <zajic815@gmail.com>
2022-03-26 22:01:37 +0100

    fix social icons header display

--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -36,7 +36,8 @@ export const Div2 = styled.div`
 `;
 export const Div3 = styled.div`
   grid-area: 1 / 5 / 2 / 6;
-  display: flex;
+  display: block;
+  /* display: flex; */
   justify-content: space-around;
   align-items: center;
   @media ${(props) => props.theme.breakpoints.sm} {
@@ -107,18 +108,16 @@ export const NavProductsIcon = styled(IoIosArrowDropdown)`
   }
 `;
 
-
-// Social Icons 
+// Social Icons
 
 export const SocialIcons = styled.a`
-transition: 0.3s ease;
-color: white;
-border-radius: 50px;
+  transition: 0.3s ease;
+  color: white;
+  border-radius: 50px;
   padding: 8px;
-&:hover {
+  &:hover {
     background-color: #212d45;
     transform: scale(1.2);
     cursor: pointer;
-    
   }
-`
+`;
