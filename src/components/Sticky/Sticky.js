bce8a001a4582ab06e368084f1d4893d1a816bd5
ZaionCode <zajic815@gmail.com>
2022-04-17 19:13:48 +0200

    add sticky

---
+++ src/components/Sticky/Sticky.js
@@ -0,0 +1,25 @@
+import { Section, SectionTitle, SectionText } from '../../styles/GlobalComponents';
+import { StickyBar, StickyBtn, BtnCont } from './StickyStyles';
+
+const Sticky = ({ title, repo, download }) => (
+  <StickyBar>
+    <BtnCont>
+      {download ? (
+        <StickyBtn href={download} target="_blank">
+          Download
+        </StickyBtn>
+      ) : (
+        ''
+      )}
+      {repo ? (
+        <StickyBtn href={repo} target="_blank">
+          Repo
+        </StickyBtn>
+      ) : (
+        ''
+      )}
+    </BtnCont>
+  </StickyBar>
+);
+
+export default Sticky;
---
+++ src/components/Sticky/StickyStyles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const StickyBar = styled.div`
+  position: sticky;
+  top: 0;
+  width: 100%;
+  background: #0f1624;
+  color: #ffffff;
+  height: 60px;
+  padding: 0 120px;
+  display: flex;
+  align-items: center;
+`;
+
+export const BtnCont = styled.div`
+  /* align-items: center;
+  display: flex; */
+`;
+
+export const StickyBtn = styled.a`
+  color: #d4c0c0;
+  font-size: 1.6rem;
+  padding: 1rem 1.5rem;
+  margin-right: 1rem;
+  background: #6b3030;
+  border-radius: 15px;
+  transition: 0.5s;
+  &:hover {
+    background: #801414;
+  }
+`;
