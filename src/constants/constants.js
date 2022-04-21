fb9aaad8136e70b1c291cc09a4ace4c2dc558b46
ZaionCode <zajic815@gmail.com>
2022-04-21 22:59:37 +0200

    add thumb images

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,17 +3,17 @@ export const projects = [
     title: 'Melody Spawner',
     description:
       "Melody Spawner is a Max for Live midi device for generation and development of monophonic melodies. Get inspired or create a melody outright directly inside Ableton Live's clips.",
-    image: '/images/1.png',
+    image: '/images/melodyspawner_thumb.png',
   },
   {
     title: 'Chord Spawner',
     description:
       "Chord Spawner is a Max for Live midi device for instant generation of chord progressions. Get inspired or create a chord progression outright directly inside Ableton Live's clips.",
-    image: '/images/1.png',
+    image: '/images/chordspawner_thumb.png',
   },
   {
     title: 'MS to Beats',
     description: 'A simple plugin that converts milliseconds to beats and vice versa in real time.',
-    image: '/images/3.jpg',
+    image: '/images/ms_to_beats_thumb.png',
   },
 ];
