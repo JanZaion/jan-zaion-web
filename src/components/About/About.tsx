7f68eca5d882fde4d96c5a116fd0ace930d86c93
JanZaion <zajic815@gmail.com>
2024-02-26 17:37:58 +0100

    feat: update bio

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -7,12 +7,16 @@ const About = () => (
     <h2>About</h2>
     <AboutText>
       My name is Jan and I&apos;m based in Brno, Czech Republic. Throughout my
-      career, I&apos;ve held various positions in the IT industry. I have a
-      passion for information technology, music theory and music in general. I
-      love building simple solutions to complex problems. Solutions that focus
-      on one thing, but do that one thing really well.
+      career, I&apos;ve held various positions in the IT industry. I currently
+      work at <a href="https://apitree.cz/">ApiTree</a> as a Frontend Developer.
       <br />
-      <br /> My native programming language is TypeScript. I&apos;ve worked with
+      <br />
+      I have a passion for information technology, music theory and music in
+      general. I love building simple solutions to complex problems. Solutions
+      that focus on one thing, but do that one thing really well.
+      <br />
+      <br />
+      My native programming language is TypeScript. I&apos;ve worked with
       various technologies including React, Next, Node, Max and Max for Live.
     </AboutText>
   </Section>
