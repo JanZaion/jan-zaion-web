48704f215fb155cea3a8efdf3a648262cba346d9
Jan Zaion <62064996+JanZaion@users.noreply.github.com>
2025-01-08 18:06:50 +0100

    feat: deprecate chordspawner

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -18,12 +18,6 @@ export const projects = [
     image: '/images/melodyspawner_thumb.png',
   },
   {
-    title: 'Chord Spawner',
-    description:
-      "Chord Spawner is a Max for Live MIDI device for instant generation of chord progressions. Get inspired or create a chord progression outright directly inside Ableton Live's clips.",
-    image: '/images/chordspawner_thumb.png',
-  },
-  {
     title: 'MS to Beats',
     description:
       'A simple plugin that converts milliseconds to beats and vice versa in real time.',
